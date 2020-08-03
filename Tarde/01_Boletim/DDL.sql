84930c1d87abcdaf0f1cd763ca00b91a0d9e3541
Paulo Brandão <paulobrandaoofficial@gmail.com>
2020-08-03 15:23:40 -0300

    Aula 06 da Tarde Finalizada

---
+++ Tarde/01_Boletim/DDL.sql
@@ -0,0 +1,51 @@
+/* DDL - Data Definition Language */
+-- Criar o banco
+CREATE DATABASE boletim;
+GO
+-- Usar efetivamente o banco
+USE boletim;
+GO
+-- Apagamos uma base de dados
+-- DROP DATABASE boletim;
+
+/* 
+	Criamos a tabela Aluno 
+	IDENTITY - Gera os ids automaticamente, incrementando-os
+*/
+CREATE TABLE Aluno (
+	IdAluno INT PRIMARY KEY IDENTITY NOT NULL,
+	Nome VARCHAR(100) NOT NULL,
+	Ra VARCHAR(20),
+	Idade INT
+);
+GO
+
+/* Criamos a tabela Materia */
+CREATE TABLE Materia (
+	IdMateria INT IDENTITY PRIMARY KEY NOT NULL,
+	Titulo VARCHAR(50)
+);
+GO
+
+
+CREATE TABLE Trabalho (
+	IdTrabalho INT IDENTITY PRIMARY KEY NOT NULL,
+	Nota DECIMAL,
+
+	-- Colocamos as chaves estrangeiras (FK)
+	IdMateria INT FOREIGN KEY REFERENCES Materia(IdMateria),
+	IdAluno	  INT FOREIGN KEY REFERENCES Aluno(IdAluno)
+);
+GO
+
+-- Apagamos uma tabela caso necessário
+-- DROP TABLE Aluno;
+-- DROP TABLE Materia;
+-- DROP TABLE Trabalho;
+
+-- Alterar a estrutura da tabela Trabalho, incluindo uma coluna nova
+ALTER TABLE Trabalho ADD DataEntrega DATETIME;
+
+-- Alteramos e ecluímos uma coluna de teste
+-- ALTER TABLE Trabalho ADD Teste INT;
+-- ALTER TABLE Trabalho DROP COLUMN Teste;
